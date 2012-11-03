64bb330845fd111f5040a3bb36aeba3db166b5b7
Vladimir Brasil <vladimir.brasil@gmail.com>
2012-11-03 15:32:58 -0200

    Inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leis3::Application.config.secret_token = 'eea1c37570a3ac51cd8fbe6a503acab4e0b63691dd3fb1d8782c4b37b2f88cf4bd1fddca974fc11b018f8382178d55ebfd0c1ada35ef7a9832842fa77ec641a2'
